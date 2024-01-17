f40df027cc49847e0bf2b67d6afac36604dac152
mihec109 <miha.hirtl@gmail.com>
2024-01-17 02:19:10 +0100

    Prijatelji
    Prijatelji footer-header

---
+++ PrijateljiPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace blitz_chat;
+
+public partial class PrijateljiPage : ContentPage
+{
+	public PrijateljiPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void BlitzchatClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainMenu());
+
+    }
+
+    private async void OnIzpisButtonClicked(object sender, EventArgs e)
+    {
+        bool result = await DisplayAlert("Pozor!", "Ste prepričani da se želite izpisati?", "Da", "Ne");
+
+        if (result)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+        else
+        {
+
+        }
+    }
+
+    private async void OnProfileTab(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProfilePage());
+    }
+}
